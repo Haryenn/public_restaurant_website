a545b43ccf7163992c0ca2052217c24aec0fe41b
Haryenn <marvingbokogounou@gmail.com>
2026-01-12 00:27:22 +0100

    change lorem paragraph

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -173,7 +173,7 @@
                       Fast Food Restaurant
                     </h1>
                     <p>
-                  repellat dolore, iste magni quos nihil ducimus libero ipsam.
+                  dolore, iste magni quos nihil ducimus libero ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
