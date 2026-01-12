06300833baa25866d12120f7c90d49a22af27182
Haryenn <marvingbokogounou@gmail.com>
2026-01-12 20:17:47 +0100

    change paragraph for better comprehension

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -173,7 +173,7 @@
                       Fast Food Restaurant
                     </h1>
                     <p>
-                  hil ducimus libero ipsam.
+                  il ducimus libero ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
