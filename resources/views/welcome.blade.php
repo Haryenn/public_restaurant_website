d2d6f1551bdaea980637d39c07bad0ef776b9bce
Haryenn <marvingbokogounou@gmail.com>
2026-01-12 20:18:28 +0100

    change paragraph for better comprehension

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -173,7 +173,7 @@
                       Fast Food Restaurant
                     </h1>
                     <p>
-                  ucimus libero ipsam.
+                  cimus libero ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
