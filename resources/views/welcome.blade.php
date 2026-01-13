ec06eda383e049c83519c44ee73171001bd62e89
Haryenn <marvingbokogounou@gmail.com>
2026-01-13 22:24:46 +0100

    change paragraph for better comprehension

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -173,7 +173,7 @@
                       Fast Food Restaurant
                     </h1>
                     <p>
-                  am.
+                  
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
