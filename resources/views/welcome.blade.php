7adbc81afe01932aac7adc8507df9b4b0d4d5340
Haryenn <marvingbokogounou@gmail.com>
2026-01-19 21:54:35 +0100

    delete lorem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -194,7 +194,7 @@
                       Fast Food Restaurant
                     </h1>
                     <p>
-                   que, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
+                   ue, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
