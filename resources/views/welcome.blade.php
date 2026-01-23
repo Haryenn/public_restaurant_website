a53b064da4587d34fb42ecb6d385f3ac1cea9d8f
Haryenn <marvingbokogounou@gmail.com>
2026-01-23 21:14:21 +0100

    delete lorem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -194,7 +194,7 @@
                       Fast Food Restaurant
                     </h1>
                     <p>
-                    cimus libero ipsam.
+                    mus libero ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
