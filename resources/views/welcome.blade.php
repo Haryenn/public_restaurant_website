16fd5fa6aeb5fd71ef236be1a8ebe8374f8fe856
Haryenn <marvingbokogounou@gmail.com>
2026-01-24 22:04:02 +0100

    remove lorem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -194,7 +194,7 @@
                       Fast Food Restaurant
                     </h1>
                     <p>
-                  ero ipsam.
+                  ro ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
