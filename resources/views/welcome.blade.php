8fca38264268537474802a90467aa835858f3cc5
Haryenn <marvingbokogounou@gmail.com>
2026-01-25 17:20:27 +0100

    remove lorem ipsum

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -194,7 +194,7 @@
                       Fast Food Restaurant
                     </h1>
                     <p>
-                   m.
+                   .
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
