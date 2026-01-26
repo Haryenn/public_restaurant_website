66667ac55b56fad8d1fd9a44de6327b182602c36
Haryenn <marvingbokogounou@gmail.com>
2026-01-26 10:16:30 +0100

    remove lorem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -402,7 +402,7 @@
                     Delicious Pizza
                   </h5>
                   <p>
-                    niam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
+                    iam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
                   </p>
                   <div class="options">
                     <h6>
