0456d4521c16bee8b03f5b919a9d9360b9443b37
Haryenn <marvingbokogounou@gmail.com>
2026-01-27 23:06:30 +0100

    del lorem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -152,7 +152,7 @@
                       Fast Food Restaurant
                     </h1>
                     <p>
-                       sse error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
+                       se error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
