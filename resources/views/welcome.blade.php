f335bd815aa7551df908e748f74027ea608e6459
Haryenn <marvingbokogounou@gmail.com>
2026-01-28 21:01:21 +0100

    del lorem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -153,7 +153,7 @@
                     </h1>
                     <p>
                       
-                      e. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
+                    Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
