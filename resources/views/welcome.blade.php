b674339f3608b087905e50f42a0441854a9fdfeb
Haryenn <marvingbokogounou@gmail.com>
2026-01-29 22:00:25 +0100

    remove lorem ipsum

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -153,7 +153,7 @@
                     </h1>
                     <p>
                       
-                     x doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
+                     doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
