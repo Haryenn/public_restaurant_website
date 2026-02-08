61e2e3e11718e2fe5a74ceabd4ed585c266e0bde
Haryenn <marvingbokogounou@gmail.com>
2026-02-08 21:39:05 +0100

    change lorem paragraph

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -153,7 +153,7 @@
                     </h1>
                     <p>
                       
-                    remque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
+                    emque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
