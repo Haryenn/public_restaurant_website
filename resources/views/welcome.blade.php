a91c763845a1b878d2deb89bcad251c25ab1c751
Haryenn <marvingbokogounou@gmail.com>
2026-02-09 20:48:43 +0100

    change lorem paragraph

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -153,7 +153,7 @@
                     </h1>
                     <p>
                       
-                    abore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
+                    bore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
                     </p>
                     <div class="btn-box">
                       <a href="" class="btn1">
